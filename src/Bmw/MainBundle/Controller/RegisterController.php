eb0f2195deb9f55c0baa0921452eff0cd669987a
Miky <mail.uek.krakow@on2.pl>
2015-02-13 13:08:21 +0100

    register up

---
+++ src/Bmw/MainBundle/Controller/RegisterController.php
@@ -0,0 +1,42 @@
+<?php
+namespace Bmw\MainBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Bmw\MainBundle\Form\UserType;
+use Bmw\MainBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+class RegisterController extends Controller
+{
+    /**
+     * @Route("/register")
+     * @Template()
+     */
+   
+    public function registerAction(Request $request)
+    {
+        
+		$user = new User();
+		
+    	$form = $this->createForm(new UserType(), $user);
+		$form->handleRequest($request);
+		
+		//trzeba dorobić walidacje
+		if ($form->isValid()) {
+   			 $user->setRole(1); //rola użytkownika
+    		 $user->setItemStatus(1);	//użytkownik aktywny
+    		 
+   			 $em = $this->getDoctrine()->getManager();
+   			 $em->persist($user);
+   			 $em->flush();
+			 //powinien zwrócić komunikat o sukcesie operacji
+   			 //return $this->redirect($this->generateUrl('user_success'));
+		}
+		
+  		
+        return array(
+				'form' => $form->createView(),
+			);
+		
+    }
+}
