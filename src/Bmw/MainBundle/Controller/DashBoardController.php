54f608335427e55684a042b5336ce425fef226e6
Bartek Lenart <barteklenartbl@gmail.com>
2015-02-13 09:54:28 +0100

    kategorie filmow

--- src/Bmw/MainBundle/Controller/DashBoardController.php
+++ src/Bmw/MainBundle/Controller/DashBoardController.php
@@ -15,14 +15,63 @@ class DashBoardController extends Controller
    
     public function indexAction()
     {
-     
+     	$category = '';
 		$repository = $this->getDoctrine()->getRepository('BmwMainBundle:Movie');
 		
-		$movie = $repository->findAll();
+		$movie = $repository->findByPrice(6.99);
 		
 		return $this->render('BmwMainBundle:DashBoard:index.html.twig', array(
-	    	'movie' => $movie
+	    	'movie' => $movie,
+	    	'category' => $category,
+	    	'title' => 'DashBoard'
 	    	));
 		
+	}
+
+	public function selectAction($type)
+	{
+		$repository = $this->getDoctrine()->getRepository('BmwMainBundle:Movie');
+		
+		$category = '';
+		if ($type == 'action'){
+			$type = 1;
+			$category = 'Akcji';
+			$movie = $repository->findBycategory($type);
+		}
+		if($type == 'comedy'){
+			$type = 2;
+			$category = 'Komedie';
+			$movie = $repository->findBycategory($type);
+		} if ($type == 'horror'){
+			$type = 3;
+			$category = 'Horror';
+			$movie = $repository->findBycategory($type);
+		} if ($type == 'scifiction'){
+			$type = 4;
+			$category = 'Fantasy';
+			$movie = $repository->findBycategory($type);
+		} if ($type == 'all'){
+			$category = 'Wszystkie';
+			$movie = $repository->findAll();
+		}
+		//exit(\Doctrine\Common\Util\Debug::dump($movie));
+		return $this->render('BmwMainBundle:DashBoard:index.html.twig', array(
+			'title' => 'Kategoria: '.$category,
+			'category' => $category,
+			'movie' => $movie
+			));
+	}
+
+	public function movieAction($page)
+	{
+		$repository = $this->getDoctrine()->getRepository('BmwMainBundle:Movie');
+		
+		$page1 = $page+8;
+		$movie = $repository->findOneBymovieId($page1);
+		//exit(\Doctrine\Common\Util\Debug::dump($movie));
+		return $this->render('BmwMainBundle:DashBoard:singleindex.html.twig', array(
+			'title' => 'Wybrany film',
+			'movie' => $movie
+			));
 	}
 }
