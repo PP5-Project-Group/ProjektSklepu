fc531cfe1a3b269ee8fdc38015a81c8995872985
Wojtas <Wojtas18m@wp.pl>
2015-02-19 12:14:10 +0100

    zamawiane bez selecta

--- src/Bmw/MainBundle/Controller/DashBoardController.php
+++ src/Bmw/MainBundle/Controller/DashBoardController.php
@@ -18,6 +18,7 @@ class DashBoardController extends Controller
     {
     	     	
      	$category = 'Most Popular';
+		$category2 = 'Most Ordered';
 		$repository = $this->getDoctrine()->getRepository('BmwMainBundle:Movie');		
 		$movie = $repository->findByPrice(6.99);
 
@@ -43,15 +44,35 @@ class DashBoardController extends Controller
 	      
 	      $stmt->execute();  
 	      $mostPopular =  $stmt->fetchAll();  
+		  
+		  $stmy = $this->getDoctrine()->getManager()  
+	                   ->getConnection()  
+	                   ->prepare(
+	                   	'SELECT
+	                   	 title, price, img_url, description, Movie.movie_id, SUM( Review.movie_id) Suma, COUNT(title) Count 
+	                   	FROM 
+	                   	 Review LEFT JOIN Movie 
+	                   	ON
+	                   	 Review.movie_id = Movie.movie_id
+	                   	 GROUP BY 
+	                   	title
+	                   	 ORDER BY
+	                   	Count DESC
+	                   	LIMIT 3'
+	                   	);  
+	      
+	      $stmy->execute();  
+	      $mostOrdered =  $stmy->fetchAll(); 
 	 
 	 // exit(\Doctrine\Common\Util\Debug::dump($mostPopular));
 
 		return $this->render('BmwMainBundle:DashBoard:index.html.twig', array(
 	    	'movie' => $movie,
 	    	'category' => $category,
+			'category2' => $category2,
 	    	'title' => 'DashBoard',
 	    	'popular' => $mostPopular,
-	    	'ordered' => 'Most Ordered'
+	    	'ordered' => $mostOrdered
 	    	));
 		
 		
